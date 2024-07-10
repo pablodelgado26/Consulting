cb6a9f3f1f47f3610611741191ee2692adefdcc2
Pablo Delgado <pablodelgado.tds2.senai@gmail.com>
2024-07-10 20:41:48 -0300

    termino php, teste

--- envia.php
+++ envia.php
@@ -3,5 +3,19 @@
     $nome = addslashes($_POST['nome']);
     $email = addslashes($_POST['email']);
     $telefone = addslashes($_POST['telefone']);
+    $mensagem = addslashes($_POST['mensagem']);
+
+    $para = "md.spconsultoria@gmail.com"
+    $assunto = "Coleta de dados site consultorias";
+
+    $corpo = "Nome: ".$nome."\n"."E-mail: ".$email."\n"." Telefone: ".$telefone."\n"."Mensagem: ".$mensagem;
+
+    $cabeca = "From: teste@formulario.com"."\r\n"."Reply-To: ".$email."\n"."X-Mailer: PHP/".phpversion();
+
+    if(mail($para, $assunto, $corpo, $cabeca, $mensagem)){
+        echo("E-mail enviado com sucesso!");
+    }else{
+        echo("O e-mail não pode ser enviado");
+    }
 
 ?>
