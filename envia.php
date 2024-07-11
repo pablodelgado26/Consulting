7f5394131baed7999b19d66d99a57b33c088d6b3
Pablo Delgado <pablodelgado.tds2.senai@gmail.com>
2024-07-11 00:02:03 -0300

    tentatica com codigo antigo

--- envia.php
+++ envia.php
@@ -1,45 +1,21 @@
 <?php
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
+    $nome = addslashes($_POST['nome']);
+    $email = addslashes($_POST['email']);
+    $telefone = addslashes($_POST['telefone']);
+    $mensagem = addslashes($_POST['mensagem']);
 
-include ("includes/config.php");
-include ("vendor/autoload.php");
+    $para = "md.spconsultoria@gmail.com"
+    $assunto = "Coleta de dados - consultorias";
 
-require 'vendor/autoload.php';
+    $corpo = "Nome: ".$nome."\n"."E-mail: ".$email."\n"." Telefone: ".$telefone."\n"."Mensagem: ".$mensagem;
 
-if (isset($_POST['Enviar'])){
-$mail = new PHPMailer(true);
+    $cabeca = "From: md.spconsultoria@gmail.com"."\r\n"."Reply-To: ".$email."\n"."X-Mailer: PHP/".phpversion();
 
-    try {                     
-    $mail->isSMTP();                                            
-    $mail->Host       = SMTP_HOST;                     
-    $mail->SMTPAuth   = true;                                   
-    $mail->Username   = SMTP_USER;                     
-    $mail->Password   = SMTP_PASS;                                         
-    $mail->Port       = SMTP_PORT;       
-
-    $mail->setFrom('md.spconsultoria@gmail.com', 'consultoria');
-    $mail->addAddress('md.spconsultoria@gmail.com', 'Mônica Delgado');    
-
-    $mail->Subject = 'mensagem site consultoria';
-    $mail->isHTML(true);
-    $body = "Mensagem enviada através do site, segue informações abaixo: <br><br>
-    nome: ". $_POST['nome']."<br>
-    email: ". $_POST['email']."<br>
-    telefone: ". $_POST['telefone']."<br>
-    mensagem: ". $_POST['mensagem'];
-
-    $mail->Body    = $body;
-
-    $mail->send();
-    echo 'E-mail enviado com sucesso!';
-    } catch (Exception $e) {
-    echo "Erro ao enviar o e-mail: {$mail->ErrorInfo}";
+    if(mail($para,$assunto,$corpo,$cabeca,$mensagem)){
+        echo("E-mail enviado com sucesso!");
+    }else{
+        echo("O e-mail não pode ser enviado");
     }
-}else{
-    echo "Erro ao enviar o e-mail, acesso não foi via formulário: {$mail->ErrorInfo}";
-}
 
 ?>
